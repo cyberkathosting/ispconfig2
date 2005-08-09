3033490a0175cd1c7b3917cb581caa716532db5e
till <till@418fc7b0-dcfb-0310-bdb1-f3812d3f35a6>
2005-08-09 10:56:45 +0000

    Allow only lowercase usernames, otherwise postfix fails
    git-svn-id: svn://svn.ispconfig.org/ispconfig/trunk@37 418fc7b0-dcfb-0310-bdb1-f3812d3f35a6

--- install_ispconfig/ispconfig/lib/plugins/isp_username.plugin.php
+++ install_ispconfig/ispconfig/lib/plugins/isp_username.plugin.php
@@ -113,8 +113,8 @@ class isp_username_plugin {
     {
     global $go_info, $go_api,$HTTP_POST_VARS,$next_tree_id;
         $plugin_vars = $HTTP_POST_VARS["plugin"];
-        $username = $plugin_vars["user"];
-    $email = $plugin_vars["email"];
+        $username = strtolower($plugin_vars["user"]);
+    	$email = $plugin_vars["email"];
 
         $sys_config = $go_api->db->queryOneRecord("SELECT * from sys_config");
         $user_prefix = $sys_config["user_prefix"];
@@ -135,7 +135,7 @@ class isp_username_plugin {
                 }
         }
         // Überprüfung Username mittels regex, bei Fehler löschen
-        if(!preg_match("/^[a-zA-Z][\w\.\-\_]{3,64}$/",$user_prefix . $username)) {
+        if(!preg_match("/^[a-z][\w\.\-\_]{3,64}$/",$user_prefix . $username)) {
                 $go_api->db->query("DELETE from isp_isp_user where doc_id = '$doc_id'");
         $go_api->db->query("DELETE from isp_nodes where doc_id = '$doc_id' and doctype_id = '$doctype_id'");
                 $go_api->errorMessage('<b>'.$go_api->lng("Feld").': Username</b><br>'.$go_api->lng("Der Username muss aus min. 4, max 32 Buchstaben oder Zahlen bestehen, wobei er mit einem Buchstaben beginnen muss.") . "<br>&nbsp;<br>");
