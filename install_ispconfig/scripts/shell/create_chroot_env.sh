76c5fcf55a6a4d9cf830467511e24b30434c8e91
till <till@418fc7b0-dcfb-0310-bdb1-f3812d3f35a6>
2007-04-15 12:29:37 +0000

    Updated SSH Chroot creation script to support debian etch.
    git-svn-id: svn://svn.ispconfig.org/ispconfig/branches/ISPConfig-2.2.stable@599 418fc7b0-dcfb-0310-bdb1-f3812d3f35a6

--- install_ispconfig/scripts/shell/create_chroot_env.sh
+++ install_ispconfig/scripts/shell/create_chroot_env.sh
@@ -5,7 +5,7 @@
 #
 
 # Here specify the apps you want into the enviroment
-APPS="/bin/bash /bin/ls /bin/mkdir /bin/mv /bin/pwd /bin/rm /usr/bin/id /usr/bin/ssh /bin/ping /usr/bin/zip /bin/tar /usr/bin/dircolors"
+APPS="/usr/bin/unzip /usr/bin/mysqldump /usr/bin/mysql /usr/lib/openssh/sftp-server /bin/bash /bin/ls /bin/mkdir /bin/mv /bin/pwd /bin/rm /usr/bin/id /usr/bin/ssh /bin/ping /usr/bin/zip /bin/tar /usr/bin/dircolors"
 
 # Sanity check
 if [ "$1" = "" ] ; then
@@ -34,21 +34,69 @@ echo "id -Gn" >> usr/bin/groups
 grep /etc/passwd -e "^root" -e "^$CHROOT_USERNAME" > etc/passwd
 grep /etc/group -e "^root" -e "^$CHROOT_USERNAME" > etc/group
 
-# Copy the apps and the related libs
-for prog in $APPS;  do
-        cp $prog ./$prog
 
-        # obtain a list of related libraryes
-        ldd $prog > /dev/null
-        if [ "$?" = 0 ] ; then
-                LIBS=`ldd $prog | awk '{ print $3 }'`
-                for l in $LIBS; do
-                        mkdir ./`dirname $l` > /dev/null 2>&1
-                        cp $l ./$l
-                done
-        fi
+if [ -x ${HOMEDIR}/ldlist ]; then 
+  mv ${HOMEDIR}/ldlist ${HOMEDIR}/ldlist.bak
+fi
+
+if [ -x ${HOMEDIR}/lddlist2 ]; then 
+  mv ${HOMEDIR}/lddlist2 ${HOMEDIR}/lddlist2.bak
+fi
+      
+for app in $APPS;  do
+  # First of all, check that this application exists
+  if [ -x $app ]; then
+    # Check that the directory exists; create it if not.
+    app_path=`echo $app | sed -e 's#\(.\+\)/[^/]\+#\1#'`
+    if ! [ -d .$app_path ]; then
+      mkdir -p .$app_path
+    fi
+
+    # If the files in the chroot are on the same file system as the
+    # original files you should be able to use hard links instead of
+    # copying the files, too. Symbolic links cannot be used, because the
+    # original files are outside the chroot.
+    cp -p $app .$app
+												
+    # get list of necessary libraries
+    ldd $app >> ${HOMEDIR}/ldlist
+  fi
+done
+														    
+# Clear out any old temporary file before we start
+if [ -e ${HOMEDIR}/ldlist2 ]; then
+  rm ${HOMEDIR}/ldlist2
+fi
+
+for libs in `cat ${HOMEDIR}/ldlist`; do
+  frst_char="`echo $libs | cut -c1`"
+  if [ "$frst_char" = "/" ]; then
+    echo "$libs" >> ${HOMEDIR}/ldlist2
+  fi
 done
 
+for lib in `cat ${HOMEDIR}/ldlist2`; do
+  mkdir -p .`dirname $lib` > /dev/null 2>&1
+
+  # If the files in the chroot are on the same file system as the original
+  # files you should be able to use hard links instead of copying the files,
+  # too. Symbolic links cannot be used, because the original files are
+  # outside the chroot.
+  cp $lib .$lib
+done
+																			    
+#
+# Now, cleanup the 2 files we created for the library list
+#
+/bin/rm -f ${HOMEDIR}/ldlist
+/bin/rm -f ${HOMEDIR}/ldlist2
+																			    
 # From some strange reason these 3 libraries are not in the ldd output, but without them
 # some stuff will not work, like usr/bin/groups
 cp /lib/libnss_compat.so.2 /lib/libnsl.so.1 /lib/libnss_files.so.2 ./lib/
+
+# mysql needs the socket in the chrooted environment
+mkdir ${HOMEDIR}/var
+mkdir ${HOMEDIR}/var/run
+mkdir ${HOMEDIR}/var/run/mysqld
+ln /var/run/mysqld/mysqld.sock ${HOMEDIR}/var/run/mysqld/mysqld.sock
