c072246e2c76cf211f0a71137b1c826821b6cea6
falko <falko@418fc7b0-dcfb-0310-bdb1-f3812d3f35a6>
2008-09-18 12:49:06 +0000

    - Changed: split server aliases into multiple ServerAlias lines if the line would otherwise be too long (see http://www.howtoforge.com/forums/showthread.php?t=26869).
    git-svn-id: svn://svn.ispconfig.org/ispconfig/branches/ISPConfig-2.2.stable@1077 418fc7b0-dcfb-0310-bdb1-f3812d3f35a6

--- install_ispconfig/scripts/lib/config.lib.php
+++ install_ispconfig/scripts/lib/config.lib.php
@@ -1384,6 +1384,7 @@ Group web".$web["doc_id"];
     $sql = "SELECT isp_dep.*, isp_isp_domain.* from isp_dep,isp_isp_domain,isp_nodes where isp_dep.child_doc_id = isp_isp_domain.doc_id and isp_dep.child_doctype_id ='".$this->domain_doctype_id."' and isp_dep.parent_doctype_id = '".$this->web_doctype_id."' and isp_dep.parent_doc_id = '".$web["doc_id"]."' and isp_isp_domain.status != 'd' AND isp_nodes.doc_id = isp_isp_domain.doc_id AND isp_nodes.doctype_id = isp_isp_domain.doctype_id AND isp_nodes.status = 1";
     $domains = $mod->db->queryAllRecords($sql);
 
+    $serveralias_arr = array();
     $serveralias = "ServerAlias ";
     $rewrite_rule = "RewriteEngine on";
     foreach($domains as $domain){
@@ -1391,6 +1392,11 @@ Group web".$web["doc_id"];
           $serveralias .= $domain["domain_domain"].' ';
         } else {
           $serveralias .= $domain["domain_host"] . "." . $domain["domain_domain"].' ';
+        }
+        // split ServerAlias into multiple lines, if necessary
+        if(strlen($serveralias) > 2000){
+          $serveralias_arr[] = $serveralias;
+          $serveralias = "ServerAlias ";
         }
         if(!empty($domain["domain_weiterleitung"])){
           if($domain["domain_host"] == "") {
@@ -1412,8 +1418,13 @@ Group web".$web["doc_id"];
           }
         }
     }
-    $serveralias = substr($serveralias,0,-1);
-    if($serveralias == "ServerAlias") $serveralias = "";
+    $serveralias_arr[] = $serveralias;
+    $serveralias = '';
+
+    foreach($serveralias_arr as $s_alias){
+      if(trim($s_alias) != "ServerAlias" && substr(trim($s_alias),0,11) == "ServerAlias") $serveralias .= trim($s_alias)."\n";
+    }
+    //if(trim($serveralias) == "ServerAlias") $serveralias = "";
     if($rewrite_rule == "RewriteEngine on"){
       $rewrite_rule = "";
     } else {
