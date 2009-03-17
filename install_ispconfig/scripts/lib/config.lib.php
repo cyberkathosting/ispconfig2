e0f2461b781b846e0186ab73b1e7b06bafe695a5
falko <falko@418fc7b0-dcfb-0310-bdb1-f3812d3f35a6>
2009-03-17 15:32:16 +0000

    - Fixed problem with mod_python and .htaccess.
    git-svn-id: svn://svn.ispconfig.org/ispconfig/branches/ISPConfig-2.2.stable@1106 418fc7b0-dcfb-0310-bdb1-f3812d3f35a6

--- install_ispconfig/scripts/lib/config.lib.php
+++ install_ispconfig/scripts/lib/config.lib.php
@@ -1424,7 +1424,7 @@ Group web".$web["doc_id"];
     foreach($serveralias_arr as $s_alias){
       if(trim($s_alias) != "ServerAlias" && substr(trim($s_alias),0,11) == "ServerAlias") $serveralias .= trim($s_alias)."\n";
     }
-    
+
     if($rewrite_rule == "RewriteEngine on"){
       $rewrite_rule = "";
     } else {
@@ -1524,10 +1524,10 @@ php_admin_value session.save_path ".$mod->system->server_conf["server_path_httpd
     if($web["web_python"]){
       $python = '<IfModule mod_python.c>
  <Directory '.$document_root.'>
-   Options +Indexes +FollowSymLinks +MultiViews
-   AllowOverride Indexes AuthConfig Limit FileInfo
-   Order allow,deny
-   allow from all
+   #Options +Indexes +FollowSymLinks +MultiViews
+   #AllowOverride Indexes AuthConfig Limit FileInfo
+   #Order allow,deny
+   #allow from all
    AddHandler mod_python .py
    PythonHandler mod_python.publisher
    PythonDebug On
