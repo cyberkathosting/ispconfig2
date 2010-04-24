639fb11c9d4493ee408b2e52a319662123a960a1
till <till@418fc7b0-dcfb-0310-bdb1-f3812d3f35a6>
2010-04-24 21:05:01 +0000

    Removed deprecated code.
    git-svn-id: svn://svn.ispconfig.org/ispconfig/branches/ISPConfig-2.2.stable@1211 418fc7b0-dcfb-0310-bdb1-f3812d3f35a6

--- install_ispconfig/ispconfig/web/multidoc/navigation.php
+++ install_ispconfig/ispconfig/web/multidoc/navigation.php
@@ -40,28 +40,6 @@ $navigation = "../" . $session_design_path . "/nav.inc.php";?>
 <?include("../$session_style_path/style.css.php")?>
 // -->
 </STYLE>
-<?if($go_info["server"]["ort"] != "local"){?>
-<script language="JavaScript">
-<!--
-var bannerid;
-
-bannerid = Math.round(Math.random()*1000) + 1;
-
-function bannershow()
-
-{
-document.banner.src = "http://kansas.valueclick.com/cycle?host=hs0195481&size=468x60&b=index&noscript=1";
-setTimeout('bannershow()', 50000);
-}
-
-function bannerclick()
-
-{
-open("http://kansas.valueclick.com/redirect?host=hs0195481&size=468x60&b=index&v=0");
-}
--->
-</script>
-<?}?>
 </head>
 
 <body topmargin="0" leftmargin="0" bgcolor="<?echo $session_nav_hcolour?>" <?if($go_info["server"]["ort"] != "local") echo"onload=\"bannershow()\""?>>
